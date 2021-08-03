041090aa44250be20cd9f5a758ea073bacbc98db
Michell Excellent <theofficesmichell@gmail.com>
2021-08-03 00:55:37 -0500

    Update apiPaths.tsx

--- src/constants/apiPaths.tsx
+++ src/constants/apiPaths.tsx
@@ -6,6 +6,5 @@ export interface ApiPathsType {
 export const apiPaths: ApiPathsType = {
   themoviedbUrl: 'https://api.themoviedb.org',
   themoviedbPostersUrl: 'https://image.tmdb.org/t/p/original',
-  // themoviedbApiKey: 'e359e1ad0420182dea68eadd2515248c',
-  themoviedbApiKey: '77c6664896a34c14b128e5400af37e51',
+  themoviedbApiKey: 'e359e1ad0420182dea68eadd2515248c',
 };
